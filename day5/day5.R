6720a86f6e26cb4a9c310e0f89c73a7abd1c7666
Hernando Cortina <hch@alum.mit.edu>
2024-12-05 15:10:03 -0500

    clean up

--- day5/day5.R
+++ day5/day5.R
@@ -1,31 +1,23 @@
 library(adventdrob)
 library(tidyverse)
 input <- advent_input(day=5, year=2024)
-rules <- input[1:1176,]
-pages <- input[1178:1373,]
+rules <- input[1:1176,];  pages <- input[1178:1373,]
 rules |> separate_wider_delim(x, delim="|", names=c('before','after')) -> rules
 pages |> separate_wider_delim(x, delim=',',names_sep = 'x', too_few = 'align_start') -> pages
 
 # Part 1
-rules |> arrange(before,after) -> rules
-
 check_row <- function(row, rules) {
         row <- row[!is.na(as.numeric(row))]
         l <- length(row)
         page_checked = 1L
-
         while(page_checked < l) {
-
         i = page_checked
         while (i<l){
-
         check <- nrow(filter(rules, before==as.numeric(row[i]), after==as.numeric(row[i+1])))
         if (check==0) return(FALSE)
         i <- i+1  }
         page_checked <- page_checked + 1 }
-
-        return(TRUE)
-}
+        return(TRUE) }
 
 checked <- logical(nrow(pages))
 len <- numeric(nrow(pages))
@@ -47,19 +39,14 @@ fix_row <- function(row, rules) {
   row <- row[!is.na(as.numeric(row))]
   l <- length(row)
   page_checked = 1L
-
   while(page_checked < l) {
-
     i = page_checked
     while (i<l){
-
       check <- nrow(filter(rules, before==as.numeric(row[i]), after==as.numeric(row[i+1])))
       if (check==0) {temp <- row[i]; row[i]<-row[i+1]; row[i+1]<-temp; page_checked=1L; i=0}
       i <- i+1  }
     page_checked <- page_checked + 1 }
-
-  return(as.numeric(row))
-}
+  return(as.numeric(row)) }
 
 fixed <- list()
 for (i in 1:nrow(failed)) fixed[[i]] <- fix_row(failed[i,], rules)
