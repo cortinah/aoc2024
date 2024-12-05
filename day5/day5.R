74ca9c22cf2fdd5cddc4213e415d695a1b7ea4a6
Hernando Cortina <hch@alum.mit.edu>
2024-12-05 01:38:35 -0500

    day5 part1

---
+++ day5/day5.R
@@ -0,0 +1,62 @@
+library(adventdrob)
+library(tidyverse)
+input <- advent_input(day=5, year=2024)
+rules <- input[1:1176,]
+pages <- input[1178:1373,]
+rules |> separate_wider_delim(x, delim="|", names=c('before','after')) -> rules
+pages |> separate_wider_delim(x, delim=',',names_sep = 'x', too_few = 'align_start') -> pages
+
+# Part 1
+rules |> arrange(before,after) -> rules
+
+check_row <- function(row, rules) {
+        row <- row[!is.na(as.numeric(row))]
+        l <- length(row)
+        page_checked = 1L
+
+        while(page_checked < l) {
+
+        i = page_checked
+        while (i<l){
+
+        check <- nrow(filter(rules, before==as.numeric(row[i]), after==as.numeric(row[i+1])))
+        if (check==0) return(FALSE)
+        i <- i+1  }
+        page_checked <- page_checked + 1 }
+
+        return(TRUE)
+}
+
+checked <- logical(nrow(pages))
+len <- numeric(nrow(pages))
+for (i in 1:nrow(pages)) checked[i] <- check_row(pages[i,], rules)
+for (i in 1:nrow(pages)) len[i] <- length(pages[i,!is.na(as.numeric(pages[i,]))])
+len <- ceiling(len/2)
+pages <- cbind(pages, len)
+
+passed <- pages[checked,]
+sum <- 0
+for (i in 1:nrow(passed)) sum <- sum + as.numeric(passed[i, passed[i,"len"]])
+
+
+# Part 2
+failed <- pages[!checked,]
+
+fix_row <- function(row, rules) {
+  row <- row[!is.na(as.numeric(row))]
+  l <- length(row)
+  page_checked = 1L
+
+  while(page_checked < l) {
+
+    i = page_checked
+    while (i<l){
+
+      check <- nrow(filter(rules, before==as.numeric(row[i]), after==as.numeric(row[i+1])))
+      if (check==0) {temp <- row[i]; row[i]<-row[i+1]; row[i+1]<-temp}
+      i <- i+1  }
+    page_checked <- page_checked + 1 }
+
+  return(row)
+}
+failed$len<-NULL
